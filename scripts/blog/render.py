f2c89bbe1c22ef86d2da053a2e7c9f6bf53f88d6
bergi <s.berke@netzkolchose.de>
2025-08-25 10:15:15 +0200

    blog: do not code-highlight markdown blocks without language setting + rerender old posts + add `llmchat` as specially rendered language to blog renderer

--- scripts/blog/render.py
+++ scripts/blog/render.py
@@ -1,5 +1,7 @@
+import re
 from pathlib import Path
-from typing import Dict, Any
+import html as html_lib
+from typing import Dict, Any, List, Tuple
 
 import marko
 import marko.inline
@@ -51,6 +53,11 @@ class HTMLRenderer(_get_renderer_base_class()):
     def __init__(self, link_mapping: Dict[str, str], **kwargs):
         super().__init__(**kwargs)
         self._link_mapping = link_mapping
+        self._generated_id = 1022
+
+    def generate_id(self):
+        self._generated_id += 1
+        return f"subgenius-{self._generated_id}"
 
     def render_heading(self, element: marko.block.Heading) -> str:
         if hasattr(element, "anchor"):
@@ -82,9 +89,127 @@ class HTMLRenderer(_get_renderer_base_class()):
         html = super().render_table(element)
         return f"""<div style="overflow: scroll;">{html}</div>"""
 
-    def render_fenced_code(self, element):
-        html = super().render_fenced_code(element)
+    def render_fenced_code(self, element: marko.block.FencedCode):
+        if element.lang.startswith("llmchat"):
+            return self._render_llm_chat(element)
+        if element.lang:
+            html = super().render_fenced_code(element)
+        else:
+            html = f"<pre>{html_lib.escape(element.children[0].children)}</pre>"
         return f"""<div style="overflow: scroll;">{html}</div>"""
+
+    def _render_llm_chat(self, element: marko.block.FencedCode):
+        all_open = "+all" in element.lang
+        system_open = "+system" in element.lang
+        tools_open = "+tools" in element.lang
+        chat_text: str = element.children[0].children
+        sections = _split_chat_text(chat_text)
+        htmls = []
+        for role, text in sections:
+            checkbox_id = self.generate_id()
+            block_open = (
+                all_open
+                or role in ("user", "assistant", "tool_call")
+                or (role == "system" and system_open)
+                or (role == "available_tools" and tools_open)
+            )
+            htmls.append(
+                """
+                <label for="{checkbox_id}">
+                    <div class="llm-chat-window llm-chat-role-{role}">
+                        <input id="{checkbox_id}" type="checkbox" {checked_attr}>
+                        <div class="llm-chat-window-title">{role}</div>
+                        <div class="llm-chat-window-content">{html}</div>
+                    </div>
+                </label>
+                """.format(
+                    role=role,
+                    html=self._render_llm_chat_block(text),
+                    checkbox_id=checkbox_id,
+                    checked_attr='checked=""' if block_open else "",
+                )
+            )
+
+        html = """
+            <div class="llm-chat">
+                {windows}
+                <label class="llm-chat-full-transcript">
+                    <input id="{checkbox_id}" type="checkbox">
+                    <div class="llm-chat-label-show">Show plain text</div>
+                    <pre>{full_transcript}</pre>
+                </label>
+            </div>
+        """.format(
+            windows="\n".join(htmls),
+            checkbox_id=self.generate_id(),
+            full_transcript=html_lib.escape(chat_text.replace("\\```", "```")),
+        )
+        # print("X", html)
+        return html
+
+    def _render_llm_chat_block(self, text: str) -> str:
+        blocks = re.split(r"\\```", text)
+        is_code_block = False
+        htmls = []
+        for block in blocks:
+            block = html_lib.escape(block)
+            if is_code_block:
+                html = f"""<pre>```{block.rstrip()}\n```</pre>"""
+            else:
+                html = block.strip()
+            htmls.append(html)
+            is_code_block = not is_code_block
+        return "".join(htmls)
+
+def _split_chat_text(text: str) -> List[Tuple[str, str]]:
+    special_tokens = {
+        "<|start_of_role|>": "role",
+        "<|end_of_role|>": "text",
+        "<|end_of_text|>": "text",
+        "<|tool_call|>": "tool_call",
+    }
+    parsed_text = ""
+    blocks = [{"block": "text", "text": ""}]
+    for c in text:
+        parsed_text += c
+
+        is_new_block = False
+        for special_token, next_block_type in special_tokens.items():
+            if parsed_text.endswith(special_token):
+                # hack: catch special case in system prompt
+                if special_token == "<|tool_call|>" and parsed_text.endswith("respond only with <|tool_call|>"):
+                    continue
+                blocks[-1]["text"] = blocks[-1]["text"][:-len(special_token)+1]
+                if blocks[-1]["block"] == "role":
+                    blocks[-1]["text"] += f"|{len(blocks)}"
+                blocks.append({"block": next_block_type, "text": ""})
+                is_new_block = True
+                break
+
+        if not is_new_block:
+            blocks[-1]["text"] += c
+
+    sections = []
+    section_type = "raw"
+    for block in blocks:
+        if not block["text"].strip():
+            continue
+
+        if block["block"] == "role":
+            section_type = block["text"]
+        elif block["block"] != "text":
+            section_type = block["block"]
+
+        if not sections or sections[-1]["type"] != section_type:
+            sections.append({"type": section_type.split("|")[0], "text": ""})
+
+        if block["block"] != "role":
+            sections[-1]["text"] += block["text"]
+
+    return [(s["type"], s["text"]) for s in sections if s["text"]]
+
+
+
 
 
 class HTMLTeaserRenderer(GFMRendererMixin, marko.HTMLRenderer):
